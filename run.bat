cb4348a221cf5be73af89bbd4ed2bc6dcab83aa9
guos <sunshine.guo@pdabc.com>
2019-09-09 11:26:04 +0800

    优化mpper.xml

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
 @echo off
-java -jar mybatis-generator-core-2.0.0.jar -configfile generatorConfig.xml -overwrite  %jar鍖呬綅缃涓敤&杩炴帴%
-pause    %闃叉鎵ц瀹屽悗闂繃%      
+set/p model=请输入要生成的模块名：
+if "%model%" =="" echo 模块名不能为空！
+%jar包位置多个用&连接%
+java -jar mybatis-generator-core-2.0.0.jar -configfile "%model%"-generatorConfig.xml -overwrite  
+pause     %防止执行完后闪过%         
