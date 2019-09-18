c5900a38a4e5ec294d04b75b80114db6c5d64698
guos <sunshine.guo@pdabc.com>
2019-09-18 14:56:46 +0800

    去掉cache

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-set/p model=请输入要生成的模块名：
-if "%model%" =="" echo 模块名不能为空！
+set/p model=请输入要生成的文件(后缀-generatorConfig.xml)名前缀：
+if "%model%" =="" echo 文件名前缀不能为空！
 %jar包位置多个用&连接%
 java -jar mybatis-generator-core-2.0.0.jar -configfile "%model%"-generatorConfig.xml -overwrite  
 pause     %防止执行完后闪过%         
